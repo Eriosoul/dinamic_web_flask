7aac8b97e4ffcfdf4b4e2062d04ab979206ba9d2
Eriosoul <tboss1780@gmail.com>
2023-11-17 20:29:27 +0100

    adding a db sqlalchemy from flask

--- flask_back.py
+++ flask_back.py
@@ -1,15 +1,27 @@
 from flask import Flask, render_template, url_for, redirect, request, session, flash
-# time to token expire
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = "hello"
+app.secret_key = "your_secret_key"  # Cambiar esto por una clave segura
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=0.2)
+
+db = SQLAlchemy(app)
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
@@ -17,28 +29,38 @@ def login():
         session.permanent = True
         user = request.form["nm"]
         session["user"] = user
-        flash(f"Login succesfull, {user}", "info")
+        flash(f"Login successful, {user}", "info")
         return redirect(url_for("user"))
     else:
         if "user" in session:
-            flash(f"Aldready logged in!")
+            flash("Already logged in!", "info")
             return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route("/user", methods=["GET"])
+@app.route("/user", methods=["POST", "GET"])
 def user():
+    email = None
     if "user" in session:
         user = session["user"]
-        flash(f"You are not logged in!")
-        return render_template("user.html", user=user)
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            flash(f"Email successful, {email}", "info")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
     else:
+        flash("You are not logged in!", "error")
         return redirect(url_for("login"))
 
 @app.route("/logout")
 def logout():
-    flash(f"Logout sucessfull")
+    flash("Logout successful", "info")
     session.pop("user", None)
+    session.pop("email", None)
     return redirect(url_for("login"))
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
